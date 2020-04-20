e85e03deb7d4306e576e4dcc186db1d716ac94ca
alistair garioch <alistairgarioch@gmail.com>
2020-04-20 21:01:00 +0100

    updated home page formatting

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Pet from "./Pet";
 
 function App() {
   return (
-    <div className="flex flex-wrap m-8">
-      <Pet name="Ben" animal="Dog" breed="Husky" />
-      <Pet name="Bogart" animal="Cat" breed="Mixed" />
-      <Pet name="Florence" animal="Cat" breed="Persian" />
-      <Pet name="Lila" animal="Cat" breed="British Shorthair" />
+    <div>
+      <div className="pets grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-screen-xl">
+        <Pet name="Ben" animal="Dog" breed="Husky" />
+        <Pet name="Bogart" animal="Cat" breed="Mixed" />
+        <Pet name="Florence" animal="Cat" breed="Persian" />
+        <Pet name="Lila" animal="Cat" breed="British Shorthair" />
+      </div>
     </div>
   );
 }
 
-render(React.createElement(App, {}, null), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
