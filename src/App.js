f465cc82c7f617222e4f37d4f71d93758f6d8a33
alistair garioch <alistairgarioch@gmail.com>
2020-04-23 22:17:23 +0100

    Add reach router and create details page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 function App() {
-  return <SearchParams />;
+  return (
+    <Router>
+      <SearchParams path="/" />
+      <Details path="/details/:id" />
+    </Router>
+  );
 }
 
 render(<App />, document.getElementById("root"));
